2854686df7a6aaba8bf0c43a0db0b5fa4e1bf19f
Zarif <uzbzarif@gmail.com>
2019-05-15 18:27:46 +0900

    add Magnitude and normalized

--- vector.py
+++ vector.py
@@ -19,3 +19,22 @@ class Vector():
     def __eq__(self,v):
         return self.coordinates == v.coordinates
 
+    # The magnitude of a vector is the length of the vector
+    def magnitude(self):
+        return math.sqrt(sum(i*i for i in self.coordinates))
+    # Direction 
+    def normalized(self):
+        try:
+            magnitude = self.magnitude()
+            return self.times_scalar(1./magnitude)
+        except ZeroDivisionError:
+            raise except('Cannot normalize the zero vector')
+        
+v1 = Vector([-0.221,7.437])
+v2 = Vector([8.813,-1.331,-6.247])
+v3 = Vector([5.581,-2.136])
+v4 = Vector([1.996,3.108,-4.554])
+print(v1.magnitude())
+print(v2.magnitude())
+print(v3.magnitude())
+print("v4 of direction:{}".format(v4.direction()))
